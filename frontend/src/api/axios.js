970599f8642177651ec7b7ec38f62e62291828de
manikdhiman <manikdhiman2005@gmail.com>
2026-09-01 00:10:43 +0530

    added the backend link to axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
 });
 
-api.interceptors.request.use((config) => {
+// Attach token interceptor if present
+API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
@@ -12,4 +13,4 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-export default api;
+export default API;
